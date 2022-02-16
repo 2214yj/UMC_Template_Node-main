add837bd489b253129a2e607091a1f35f5fcb07b
2214yj <2214yjgm@gmail.com>
2022-02-16 22:55:54 +0900

    CHORE : 데이터베이스 세팅 및 .gitignore 수정

--- config/database.js
+++ config/database.js
@@ -3,11 +3,11 @@ const {logger} = require('./winston');
 
 // TODO: 본인의 DB 계정 입력
 const pool = mysql.createPool({
-    host: '',
-    user: '',
+    host: 'test123.clfslonrjvfx.ap-northeast-2.rds.amazonaws.com',
+    user: 'admin',
     port: '3306',
-    password: '',
-    database: ''
+    password: '12345678',
+    database: 'test123'
 });
 
 module.exports = {
